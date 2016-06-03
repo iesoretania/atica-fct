756135c23cdba7bb2c17c6558919a5f2c2794539
Luis Ramón López <lrlopez@gmail.com>
2016-06-03 14:38:51 +0200

    Colocado botón de registrar y modificar visitas

--- src/AppBundle/Entity/WorkcenterRepository.php
+++ src/AppBundle/Entity/WorkcenterRepository.php
@@ -19,9 +19,9 @@ class WorkcenterRepository extends EntityRepository
         return $em->createQuery('SELECT DISTINCT w,
               (SELECT COUNT(a1) FROM AppBundle:Agreement a1 WHERE a1.workcenter = w AND a1.educationalTutor = :user),
               (SELECT COUNT(DISTINCT a2.student) FROM AppBundle:Agreement a2 WHERE a2.workcenter = w AND a2.educationalTutor = :user),
-              (SELECT COUNT(v3) FROM AppBundle:Visit v3 WHERE v3.workcenter = w AND v3.teacher = :user),
-              (SELECT MAX(v4.when) FROM AppBundle:Visit v4 WHERE v4.workcenter = w AND v4.teacher = :user),
-              (SELECT MIN(v5.when) FROM AppBundle:Visit v5 WHERE v5.workcenter = w AND v5.teacher = :user)
+              (SELECT COUNT(v3) FROM AppBundle:Visit v3 WHERE v3.workcenter = w AND v3.tutor = :user),
+              (SELECT MAX(v4.when) FROM AppBundle:Visit v4 WHERE v4.workcenter = w AND v4.tutor = :user),
+              (SELECT MIN(v5.when) FROM AppBundle:Visit v5 WHERE v5.workcenter = w AND v5.tutor = :user)
               FROM AppBundle:Workcenter w JOIN AppBundle:Agreement a WITH a.workcenter = w JOIN AppBundle:Company c WHERE a.educationalTutor = :user ORDER BY c.name, w.name')
             ->setParameter('user', $user)
             ->getResult();
